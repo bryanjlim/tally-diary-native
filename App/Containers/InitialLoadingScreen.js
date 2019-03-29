322612df19e39ede0e244213a363fa5f6c69c4f4
Bryan Lim <bryanlim@cs.washington.edu>
2019-03-28 18:23:00 -0700

    Sort entries on launch

--- App/Containers/InitialLoadingScreen.js
+++ App/Containers/InitialLoadingScreen.js
@@ -13,25 +13,29 @@ class LaunchScreen extends Component {
   componentDidMount() {
     const { replace } = this.props.navigation;
     DriveHelper.getFileList(this.props.accessToken).then((files) => {
-        if(files.length == 0) {
-            // New User
-            replace("UserSetupScreen");
-        } else {
-            // Returning User
-            const preferencesId = files[0].name == '0' ? files[0].id : files[1].id;
-            const entriesId = files[0].name == '0' ? files[1].id : files[0].id;
+      if (files.length == 0) {
+        // New User
+        replace("UserSetupScreen");
+      } else {
+        // Returning User
+        const preferencesId = files[0].name == '0' ? files[0].id : files[1].id;
+        const entriesId = files[0].name == '0' ? files[1].id : files[0].id;
 
-            this.props.setPreferencesId(preferencesId);
-            this.props.setEntriesId(entriesId);
+        this.props.setPreferencesId(preferencesId);
+        this.props.setEntriesId(entriesId);
 
-            DriveHelper.getFileById(this.props.accessToken, preferencesId).then((preferences) => {
-                this.props.updatePreferences(JSON.parse(preferences));
-                DriveHelper.getFileById(this.props.accessToken, entriesId).then((entries) => {
-                    this.props.updateEntries(JSON.parse(entries));
-                    replace("DrawerNavigator");
-                });
-            });
-        }
+        DriveHelper.getFileById(this.props.accessToken, preferencesId).then((preferences) => {
+          this.props.updatePreferences(JSON.parse(preferences));
+          DriveHelper.getFileById(this.props.accessToken, entriesId).then((entries) => {
+            let entriesArray = JSON.parse(entries);
+            entriesArray.sort((a, b) => {
+              return new Date(b.date).getTime() - new Date(a.date).getTime();
+            })
+            this.props.updateEntries(entriesArray);
+            replace("DrawerNavigator");
+          });
+        });
+      }
     })
   }
 
@@ -45,10 +49,10 @@ class LaunchScreen extends Component {
 }
 
 const mapStateToProps = (store) => {
-  return {accessToken: store.userInfo.userInfo.accessToken}
+  return { accessToken: store.userInfo.userInfo.accessToken }
 }
 
 export default connect(
-    mapStateToProps,
+  mapStateToProps,
   { updatePreferences, updateEntries, setPreferencesId, setEntriesId }
 )(LaunchScreen)
